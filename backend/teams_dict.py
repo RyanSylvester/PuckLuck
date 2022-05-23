49d755d76229f0e0bb6dc61124629328f3c6858d
Ryan Sylvester <ryansylvester21@gmail.com>
2022-05-22 21:09:08 -0400

    finished writing basic teams dict

--- backend/teams_dict.py
+++ backend/teams_dict.py
@@ -47,4 +47,153 @@ teams = {
         'city': 'Detroit', 
         'division': 'Atlantic'
     },
+
+    # Metro
+    'NYR': 
+    {
+        'city': 'New York', 
+        'division': 'Metropolitan'
+    },
+
+    'NYI': 
+    {
+        'city': 'New York', 
+        'division': 'Metropolitan'
+    },
+
+    'CAR': 
+    {
+        'city': 'Carolina', 
+        'division': 'Metropolitan'
+    },
+
+    'PIT': 
+    {
+        'city': 'Pittsburgh', 
+        'division': 'Metropolitan'
+    },
+    
+    'WAS': 
+    {
+        'city': 'Washington', 
+        'division': 'Metropolitan'
+    },
+    
+    'CBJ': 
+    {
+        'city': 'Columbus', 
+        'division': 'Metropolitan'
+    },
+
+    'NJ': 
+    {
+        'city': 'New Jersey', 
+        'division': 'Metropolitan'
+    },
+
+    'PHI': 
+    {
+        'city': 'Philadelpha', 
+        'division': 'Metropolitan'
+    },
+
+
+# Western
+    # Central
+    'COL': 
+    {
+        'city': 'Colorado', 
+        'division': 'Central'
+    },
+
+    'MIN': 
+    {
+        'city': 'Minnesota', 
+        'division': 'Central'
+    },
+
+    'STL': 
+    {
+        'city': 'St Louis', 
+        'division': 'Central'
+    },
+
+    'NAS': 
+    {
+        'city': 'Nashville', 
+        'division': 'Central'
+    },
+    
+    'DAL': 
+    {
+        'city': 'Dallas', 
+        'division': 'Central'
+    },
+    
+    'WPG': 
+    {
+        'city': 'Winnipeg', 
+        'division': 'Central'
+    },
+
+    'CHI': 
+    {
+        'city': 'Chicago', 
+        'division': 'Central'
+    },
+
+    'ARI': 
+    {
+        'city': 'Arizona', 
+        'division': 'Central'
+    },
+
+    # Pacific
+    'CGY': 
+    {
+        'city': 'Calgary', 
+        'division': 'Pacific'
+    },
+
+    'EDM': 
+    {
+        'city': 'Edmonton', 
+        'division': 'Pacific'
+    },
+
+    'LA': 
+    {
+        'city': 'Los Angeles', 
+        'division': 'Pacific'
+    },
+
+    'VGK': 
+    {
+        'city': 'Las Vegas', 
+        'division': 'Pacific'
+    },
+    
+    'VAN': 
+    {
+        'city': 'Vancouver', 
+        'division': 'Pacific'
+    },
+    
+    'SJ': 
+    {
+        'city': 'San Jose', 
+        'division': 'Pacific'
+    },
+
+    'ANA': 
+    {
+        'city': 'Anaheim', 
+        'division': 'Pacific'
+    },
+
+    'SEA': 
+    {
+        'city': 'Seattle', 
+        'division': 'Pacific'
+    },
 }
