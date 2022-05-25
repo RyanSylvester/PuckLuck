5351984784069747df648b144b5c980ae418c8df
Ryan Sylvester <ryansylvester21@gmail.com>
2022-05-25 13:33:27 -0400

    created a parsed season dataframe

--- backend/simulation.py
+++ backend/simulation.py
@@ -1,4 +1,5 @@
 from  teams_dict import teams
+import pandas as pd
 import random
 
 class Game:
@@ -8,15 +9,17 @@ class Game:
         self.result = random.randint(1,100)
         print(f"-----   We have a matchup between teams: {self.T1.city} and {self.T2.city}   -----")
         self.T1Prob = (Team1.strength / (Team1.strength + Team2.strength)) * 100
-        print(f"Chance of {self.T1.ab} winning: {round(self.T1Prob)}%")
-        print(f"Chance of {self.T2.ab} winning: {round((100-self.T1Prob))}%")
+        # print(f"Chance of {self.T1.ab} winning: {round(self.T1Prob)}%")
+        # print(f"Chance of {self.T2.ab} winning: {round((100-self.T1Prob))}%")
 
+        self.get_winner()
+
+    def get_winner(self):
         if self.result < self.T1Prob:
             self.winner = self.T1.ab
         else:
             self.winner = self.T2.ab
-
-        print(f"The winner is {teams[self.winner]['city']}.")
+        print(f"The winner is the {teams[self.winner]['name']}.")
 
 
 class Team:
@@ -26,10 +29,40 @@ class Team:
         self.division = teams[team]['division']
         self.strength = strength
 
-def main():
-    t1 = Team('TOR', 20)
-    t2 = Team('BOS', 20)
+class Season:
+    def __init__(self, year):
+        self.year = year
+        self.get_season_data()
+        self.create_teams()
 
-    game = Game(t1, t2)
+    def get_season_data(self):
+        # Parse the CSV
+        self.schedule = pd.read_csv(f'data/schedules/{self.year}.csv')
+
+        # Setting column names
+        col_names = ['Date', 'Away', 'Home']
+        self.schedule.columns = col_names
+
+        # Convert full names to abbreviations
+        translation = {}
+        team_abb_list = list(teams)
+        for team in team_abb_list:
+            translation[(teams[team]['name'])] = team
+        
+        self.schedule = self.schedule.replace({'Home': translation})
+        self.schedule = self.schedule.replace({'Away': translation})
+
+    def create_teams(self):
+        pass
+        
+        
+        
+
+def main():
+    # t1 = Team('TOR', 20)
+    # t2 = Team('BOS', 20)
+
+    # game = Game(t1, t2)
+    year = Season(2022)
 
 main()
--- backend/teams_dict.py
+++ backend/teams_dict.py
@@ -2,48 +2,56 @@ teams = {
     # Atlantic
     'TOR': 
     {
+        'name' : 'Toronto Maple Leafs',
         'city': 'Toronto', 
         'division': 'Atlantic'
     },
 
     'MTL': 
     {
+        'name' : 'Montreal Canadiens',
         'city': 'Montreal', 
         'division': 'Atlantic'
     },
 
     'BOS': 
     {
+        'name' : 'Boston Bruins',
         'city': 'Boston', 
         'division': 'Atlantic'
     },
 
     'FLA': 
     {
+        'name' : 'Florida Panthers',
         'city': 'Florida', 
         'division': 'Atlantic'
     },
     
     'TBL': 
     {
+        'name' : 'Tampa Bay Lightning',
         'city': 'Tampa Bay', 
         'division': 'Atlantic'
     },
     
     'OTT': 
     {
+        'name' : 'Ottawa Senators',
         'city': 'Ottawa', 
         'division': 'Atlantic'
     },
 
     'BUF': 
     {
+        'name' : 'Buffalo Sabres',
         'city': 'Buffalo', 
         'division': 'Atlantic'
     },
 
     'DET': 
     {
+        'name' : 'Detroit Red Wings',
         'city': 'Detroit', 
         'division': 'Atlantic'
     },
@@ -51,49 +59,57 @@ teams = {
     # Metro
     'NYR': 
     {
+        'name' : 'New York Rangers',
         'city': 'New York', 
         'division': 'Metropolitan'
     },
 
     'NYI': 
     {
+        'name' : 'New York Islanders',
         'city': 'New York', 
         'division': 'Metropolitan'
     },
 
     'CAR': 
     {
+        'name' : 'Carolina Hurricanes',
         'city': 'Carolina', 
         'division': 'Metropolitan'
     },
 
     'PIT': 
     {
+        'name' : 'Pittsburgh Penguins',
         'city': 'Pittsburgh', 
         'division': 'Metropolitan'
     },
     
     'WAS': 
     {
+        'name' : 'Washington Capitals',
         'city': 'Washington', 
         'division': 'Metropolitan'
     },
     
     'CBJ': 
     {
+        'name' : 'Columbus Blue Jackets',
         'city': 'Columbus', 
         'division': 'Metropolitan'
     },
 
     'NJ': 
     {
+        'name' : 'New Jersey Devils',
         'city': 'New Jersey', 
         'division': 'Metropolitan'
     },
 
     'PHI': 
     {
-        'city': 'Philadelpha', 
+        'name' : 'Philadelphia Flyers',
+        'city': 'Philadelphia', 
         'division': 'Metropolitan'
     },
 
@@ -102,48 +118,56 @@ teams = {
     # Central
     'COL': 
     {
+        'name' : 'Colorado Avalanche',
         'city': 'Colorado', 
         'division': 'Central'
     },
 
     'MIN': 
     {
+        'name' : 'Minnesota Wild',
         'city': 'Minnesota', 
         'division': 'Central'
     },
 
     'STL': 
     {
+        'name' : 'St Louis Blues',
         'city': 'St Louis', 
         'division': 'Central'
     },
 
     'NAS': 
     {
+        'name' : 'Nashville Predators',
         'city': 'Nashville', 
         'division': 'Central'
     },
     
     'DAL': 
     {
+        'name' : 'Dallas Stars',
         'city': 'Dallas', 
         'division': 'Central'
     },
     
     'WPG': 
     {
+        'name' : 'Winnipeg Jets',
         'city': 'Winnipeg', 
         'division': 'Central'
     },
 
     'CHI': 
     {
+        'name' : 'Chicago Blackhawks',
         'city': 'Chicago', 
         'division': 'Central'
     },
 
     'ARI': 
     {
+        'name' : 'Arizona Coyotes',
         'city': 'Arizona', 
         'division': 'Central'
     },
@@ -151,48 +175,56 @@ teams = {
     # Pacific
     'CGY': 
     {
+        'name' : 'Calgary Flames',
         'city': 'Calgary', 
         'division': 'Pacific'
     },
 
     'EDM': 
     {
+        'name' : 'Edmonton Oilers',
         'city': 'Edmonton', 
         'division': 'Pacific'
     },
 
     'LA': 
     {
+        'name' : 'Los Angeles Kings',
         'city': 'Los Angeles', 
         'division': 'Pacific'
     },
 
     'VGK': 
     {
+        'name' : 'Las Vegas Golden Knights',
         'city': 'Las Vegas', 
         'division': 'Pacific'
     },
     
     'VAN': 
     {
+        'name' : 'Vancouver Canucks',
         'city': 'Vancouver', 
         'division': 'Pacific'
     },
     
     'SJ': 
     {
+        'name' : 'San Jose Sharks',
         'city': 'San Jose', 
         'division': 'Pacific'
     },
 
     'ANA': 
     {
+        'name' : 'Anaheim Ducks',
         'city': 'Anaheim', 
         'division': 'Pacific'
     },
 
     'SEA': 
     {
+        'name' : 'Seattle Kraken',
         'city': 'Seattle', 
         'division': 'Pacific'
     },
