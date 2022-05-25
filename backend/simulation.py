61cf7dea4ae74c65fc12b2e739396573994ff8d3
Ryan Sylvester <ryansylvester21@gmail.com>
2022-05-25 16:32:45 -0400

    season now tracks teams and updates records

--- backend/simulation.py
+++ backend/simulation.py
@@ -12,14 +12,21 @@ class Game:
         # print(f"Chance of {self.T1.ab} winning: {round(self.T1Prob)}%")
         # print(f"Chance of {self.T2.ab} winning: {round((100-self.T1Prob))}%")
 
-        self.get_winner()
+        self.handle_result()
 
-    def get_winner(self):
+    def handle_result(self):
+        # Team 1 wins in regulation
         if self.result < self.T1Prob:
-            self.winner = self.T1.ab
+            self.T1.update_record("W")
+            self.T2.update_record("L")
+            print(f"The winner is the {self.T1.name}.")
+        # Team 2 wins in regulation
         else:
-            self.winner = self.T2.ab
-        print(f"The winner is the {teams[self.winner]['name']}.")
+            self.T1.update_record("L")
+            self.T2.update_record("W")
+            print(f"The winner is the {self.T2.name}.")
+        
+        
 
 
 class Team:
@@ -27,13 +34,34 @@ class Team:
         self.ab = team
         self.city = teams[team]['city']
         self.division = teams[team]['division']
+        self.name = teams[team]['name']
+        self.GP = 0
+        self.W = 0
+        self.L = 0
+        self.OTL = 0
+        self.P = 0
         self.strength = strength
+    
+    def update_record(self, result):
+        print("It happened")
+        if result == 'W':
+            self.W += 1
+        elif result == 'L':
+            self.L += 1
+        elif result == 'OTL':
+            self.OTL += 1
+        
+        self.GP = self.W + self.L + self.OTL
+        self.P = self.W * 2 + self.OTL
+            
+
 
 class Season:
     def __init__(self, year):
         self.year = year
         self.get_season_data()
         self.create_teams()
+        self.create_standings()
 
     def get_season_data(self):
         # Parse the CSV
@@ -53,7 +81,14 @@ class Season:
         self.schedule = self.schedule.replace({'Away': translation})
 
     def create_teams(self):
-        pass
+        self.active_teams = {}
+        for team in teams:
+            self.active_teams[team] = (Team(team, 100))
+
+    def create_standings(self):
+        self.standings = pd.DataFrame()
+        
+            
         
         
         
@@ -62,7 +97,17 @@ def main():
     # t1 = Team('TOR', 20)
     # t2 = Team('BOS', 20)
 
-    # game = Game(t1, t2)
     year = Season(2022)
+    #for value in year.active_teams.items():
+        #print(value['name'])
+        #if t.division == 'Atlantic':
+         #   print(t.name)
+    
+    #print(year.active_teams['TOR'].name)
+    g = Game(year.active_teams['TOR'], year.active_teams['BOS'])
+    #print(year.active_teams)
+    # for key, team in year.active_teams.items():
+    #     #print(value.city)
+    #     print(f"{team.ab}: {team.P} points.")
 
 main()
